3763ab9dfc251bf13fb6231146268824390c271a
unalipa333 <lyndonlp333@gmail.com>
2021-07-11 12:21:11 -0700

    registration page added

---
+++ delairebaptis/users/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+# inherits from the UserCreationForm
+# add fields to the register form inside the bootstrap 'form' 
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    
+
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+
+
+
--- delairebaptis/users/views.py
+++ delairebaptis/users/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages
+from .forms import UserRegisterForm
 #UserCreationForm is a built in django function for a user register form
 # so users can log into the site
 
 def register(request):
-    form = UserCreationForm()
+    if request.method== 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Created for {username}!')
+            return redirect('blog-home')
+
+    else: 
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+#one time alerts
+# message.debug  message.info  message.success  message.warning   message.error
+
 
 
